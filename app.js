2603ccefbdfb22a4e1b296d90662d1ebe581f412
Shelby <vanhooser@ou.edu>
2015-03-27 11:34:32 -0500

    Configure fixed

--- app.js
+++ app.js
@@ -22,21 +22,20 @@ var app = module.exports = express();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {
-    layout: false
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(function(req,res,next){
-    req.db = db;
-    next();
-  });
-  app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {
+  layout: false
 });
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+app.use(express.static(__dirname + '/public'));
+
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
